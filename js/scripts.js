6f059090cfedc2fa9d90981ced72c614bac4b6f9
aysh905 <aysh905@gmail.com>
2018-03-21 18:18:17 +0500

    First commit- Structure signin-up

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+// Empty JS for your own code to be here
+$(function(){
+	$(".sec-toggle").on("click", function(e){
+		e.preventDefault();
+		var target = $(this).attr("data-target");
+		$("#first-step, #second-step").removeClass("active");
+		$(target).addClass("active");
+		$(".sec-toggle").toggleClass("active");
+	});
+});
